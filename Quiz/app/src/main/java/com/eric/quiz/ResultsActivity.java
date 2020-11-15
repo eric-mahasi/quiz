8e7ab295295ab11eaab884412998b530e24a111e
eric-mahasi <mahasieric@gmail.com>
2020-11-15 16:42:51 +0300

    Add navigation basics

--- Quiz/app/src/main/java/com/eric/quiz/ResultsActivity.java
+++ Quiz/app/src/main/java/com/eric/quiz/ResultsActivity.java
@@ -1,4 +1,25 @@
 package com.eric.quiz;
 
-public class ResultsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_four);
+        Intent intent = getIntent();
+        Button homeButton = (Button) findViewById(R.id.buttonHome);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultsActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
