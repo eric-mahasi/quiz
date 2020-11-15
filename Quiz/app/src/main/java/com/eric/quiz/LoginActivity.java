52bf2bbe3b7018128d63149fabb958984d3f90a3
eric-mahasi <mahasieric@gmail.com>
2020-11-15 15:50:56 +0300

    Navigation from login to question one

--- Quiz/app/src/main/java/com/eric/quiz/LoginActivity.java
+++ Quiz/app/src/main/java/com/eric/quiz/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.eric.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginButton = (Button) findViewById(R.id.buttonOkLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, QuestionOneActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Quiz/app/src/main/java/com/eric/quiz/QuestionTwoActivity.java
+++ Quiz/app/src/main/java/com/eric/quiz/QuestionTwoActivity.java
@@ -1,5 +1,6 @@
 package com.eric.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,5 +10,6 @@ public class QuestionTwoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_two);
+        Intent intent = getIntent();
     }
 }
